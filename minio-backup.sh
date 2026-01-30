249230b85f128ad898938195ead72092c9eebe5b
CultureLinux <cyklodev.services@gmail.com>
2026-01-30 15:28:13 +0100

    🚧 wip: backup minio

--- minio-backup.sh
+++ minio-backup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -euo pipefail
 
 # === Configuration ===
-ALIAS_NAME="minio"
+ALIAS_NAME="preprod"
 BACKUP_DIR="./minio_backup_admin"
 DATA_BACKUP_DIR="$BACKUP_DIR/buckets"
 DATA_BACKUP_DIR_ARCHIVES="./backup_archives"
@@ -21,36 +22,46 @@ echo "📝 Log sauvegardé dans : $LOG_FILE"
 echo "----------------------------"
 
 echo "🔐 Récupération des utilisateurs..."
-mc admin user list $ALIAS_NAME | awk '{print $2}' | grep -v Username | while read -r user; do
+mc admin user list $ALIAS_NAME --json | jq -r '.accessKey' | while read -r user; do
     safe_user=$(echo "$user" | tr '/ ' '_')
     echo "- Export user: $user"
     mc admin user info $ALIAS_NAME "$user" > "$BACKUP_DIR/users/${safe_user}.info"
 done
 
 echo "👥 Récupération des groupes..."
-mc admin group list $ALIAS_NAME | awk '{print $1}' | grep -v Group | while read -r group; do
-    safe_group=$(echo "$group" | tr '/ ' '_')
-    echo "- Export group: $group"
-    mc admin group info $ALIAS_NAME "$group" > "$BACKUP_DIR/groups/${safe_group}.info"
-done
+groups=$(mc admin group list $ALIAS_NAME --json | jq -r '.group')
+
+if [ -z "$groups" ]; then
+    echo "ℹ️ Aucun groupe trouvé"
+else
+    echo "$groups" | while read -r group; do
+        safe_group=$(echo "$group" | tr '/ ' '_')
+        echo "- Export group: $group"
+        mc admin group info $ALIAS_NAME "$group" > "$BACKUP_DIR/groups/${safe_group}.info"
+    done
+fi
 
 echo "📜 Récupération des policies..."
-mc admin policy list $ALIAS_NAME | awk '{print $1}' | grep -v Policy | while read -r policy; do
-    safe_policy=$(echo "$policy" | tr '/ ' '_')
-    echo "- Export policy: $policy"
-    mc admin policy info $ALIAS_NAME "$policy" > "$BACKUP_DIR/policies/${safe_policy}.json"
-done
+policies=$(mc admin policy list $ALIAS_NAME --json | jq -r '.policy')
+
+if [ -z "$policies" ]; then
+    echo "ℹ️ Aucune policy trouvée"
+else
+    echo "$policies" | while read -r policy; do
+        safe_policy=$(echo "$policy" | tr '/ ' '_')
+        echo "- Export policy: $policy"
+        mc admin policy info $ALIAS_NAME "$policy" > "$BACKUP_DIR/policies/${safe_policy}.json"
+    done
+fi
 
 echo "🔑 Récupération des service accounts (tokens)..."
-mc admin user svcacct list $ALIAS_NAME | jq -r '.[] | .user + ":" + .accessKey' | while IFS=":" read -r user access_key; do
-    safe_user=$(echo "$user" | tr '/ ' '_')
-    safe_key=$(echo "$access_key" | tr '/ ' '_')
-    echo "- Export service account for user: $user (access: $access_key)"
-    mc admin user svcacct info $ALIAS_NAME "$access_key" > "$BACKUP_DIR/service-accounts/${safe_user}_${safe_key}.info"
+mc admin user svcacct list $ALIAS_NAME admin --json | jq -r '.accessKey' | while read -r accessKey; do
+    echo "- Export service account : $accessKey"
+    mc admin user svcacct info $ALIAS_NAME "$accessKey"  --json > "$BACKUP_DIR/service-accounts/${accessKey}.info"
 done
 
 echo "🗂️  Récupération des buckets..."
-mc ls $ALIAS_NAME | awk '{print $5}' | while read -r bucket; do
+mc ls $ALIAS_NAME --json | jq -r '.key' | while read -r bucket; do
     echo "- Sauvegarde du bucket: $bucket"
     mc mirror --overwrite "$ALIAS_NAME/$bucket" "$DATA_BACKUP_DIR/$bucket"
 done
@@ -65,3 +76,4 @@ find "$DATA_BACKUP_DIR_ARCHIVES" -name "minio_backup_*.tar.gz" -type f -mtime +7
 find "$DATA_BACKUP_DIR_ARCHIVES" -name "minio_backup_log_*.log" -type f -mtime +7 -exec rm -v {} \;
 
 echo "✅ Sauvegarde complète terminée et rotation effectuée."
+
